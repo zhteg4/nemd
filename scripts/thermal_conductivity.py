73e8dbf58d6fe82f6286449f981c5eb9273beb73
Teng Zhang <teng.zhang@schrodinger.com>
2020-05-03 12:33:14 -0400

    add parserutils.py

--- scripts/thermal_conductivity.py
+++ scripts/thermal_conductivity.py
@@ -1,8 +1,35 @@
+import sys
 import hello
+import argparse
+import parserutils
 
-def main():
-    print "Hello World!"
+FLAG_TEMP_FILE = 'temp_file'
+FlAG_ENEGER_FILE = 'energy_file'
+
+def get_parser():
+    parser = parserutils.get_parser(
+        description=
+        'Calculate thermal conductivity using non-equilibrium molecular dynamics.'
+    )
+    parser.add_argument(FLAG_TEMP_FILE,
+                        metavar=FLAG_TEMP_FILE.upper(),
+                        help='')
+    parser.add_argument(FlAG_ENEGER_FILE,
+                        metavar=FlAG_ENEGER_FILE.upper(),
+                        type=parserutils.type_file,
+                        help='')
+    return parser
+
+def validate_options(argv):
+    parser = get_parser()
+    options = parser.parse_args(argv)
+    return options
+
+def main(argv):
+
+    option = validate_options(argv)
     hello.world()
 
+
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
