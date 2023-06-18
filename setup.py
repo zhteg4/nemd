ab180664dfac13a891ef354d03ed59a62b0d460e
Yyh123456163 <458088yyh@gmail.com>
2023-06-18 16:19:58 +0800

    HTMDS-42 # package versions in setup.py

--- setup.py
+++ setup.py
@@ -27,11 +27,12 @@ setup(name='nemd',
       package_data={'nemd': ['ff/*.prm']},
       scripts=['bash_scripts/run_nemd', 'scripts/polymer_builder_driver.py'],
       install_requires=[
-          'numpy', 'scipy', 'networkx', 'pandas', 'more_itertools',
-          'chemparse', 'mendeleev', 'rdkit', 'signac', 'signac-flow', 'lammps',
-          'matplotlib', 'plotly', 'dash_bootstrap_components', 'pytest',
-          'dash[testing]', 'pyqt6', 'webdriver-manager', 'flask>=2.2.2',
-          'openpyxl', 'sh', 'humanfriendly', 'panel'
+           'numpy == 1.24.3', 'scipy == 1.10.1', 'networkx == 3.1', 'pandas == 2.0.2', 'more_itertools == 9.1.0',
+          'chemparse == 0.1.2', 'mendeleev == 0.14.0', 'rdkit == 2023.3.1', 'signac == 2.0.0', 'signac-flow == 0.25.1',
+          'lammps == 2022.6.23.4.0',
+          'matplotlib == 3.7.1', 'plotly ==5.15.0', 'dash_bootstrap_components', 'pytest ==7.3.2',
+          'dash[testing]', 'pyqt6 == 6.5.1', 'webdriver-manager == 3.8.6', 'flask>=2.2.5',
+          'openpyxl == 3.1.2', 'sh ==2.0.4', 'humanfriendly ==10.0', 'panel'
       ],
       extras_require={
           'dev': [
