eec65d1dcb8316dc8ea77c2f0807e7a2ef4d7a37
Teng Zhang <tzhang@Tengs-MacBook-Pro-2.local>
2024-02-08 15:22:10 -0500

    clean build dirs

--- practice/cmake/src/SomeLibDemo/cpp/build/CMakeFiles/mearlymath.dir/adder.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/mearlymath.dir/adder.cpp.o: \
-  /Users/tzhang/git/nemd/practice/cmake/src/libdemo/adder.cpp \
-  /Users/tzhang/git/nemd/practice/cmake/src/libdemo/adder.h
--- practice/cmake/src/SomeLibDemo2/cpp/build/CMakeFiles/mearlymath.dir/adder.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/mearlymath.dir/adder.cpp.o: \
-  /Users/tzhang/git/nemd/practice/cmake/src/SomeLibDemo2/cpp/adder.cpp \
-  /Users/tzhang/git/nemd/practice/cmake/src/SomeLibDemo2/cpp/adder.h
