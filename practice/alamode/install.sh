f2858d56409d75cc1f085d0a14e6f750cf040056
Teng Zhang <tzhang@Tengs-MacBook-Pro-2.local>
2024-02-09 12:57:00 -0500

    successfully installed alamode

--- practice/alamode/install.sh
+++ practice/alamode/install.sh
@@ -1,13 +1,26 @@
 #! /bin/zsh
+brew install gcc
+brew install lapack
+brew install open-mpi
+brew install libomp
 brew install boost
 brew install eigen
 brew install spglib
 brew install fftw
 brew install cmake
-git clone https://github.com/ttadano/alamode.git
+brew install llvm
+git submodule add https://github.com/ttadano/alamode.git
 cd alamode
 git checkout develop
-mkdir _build; cd _build
-cmake -DUSE_MKL_FFT=yes -DSPGLIB_ROOT=${SPGLIB_ROOT} \
-    -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_CXX_FLAGS="-O2 -xHOST" ..
+
+export CC=/usr/local/opt/llvm/bin/clang
+export SPGLIB_ROOT=/usr/local/Cellar/spglib/2.3.0
+export LD_LIBRARY_PATH=$SPGLIB_ROOT/lib:$LD_LIBRARY_PATH
+
+cd alm
+mkdir build; cd build
+cmake -DSPGLIB_ROOT=/usr/local/Cellar/spglib/2.3.0 -S ..
+make
+
+cmake -DUSE_MKL_FFT=yes -DSPGLIB_ROOT=/usr/local -DFFTW3_ROOT=/usr/local -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++  ..
 
