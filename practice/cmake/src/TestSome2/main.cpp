999f11fb5df21046f546be183ebc288693de969e
Teng Zhang <tzhang@Tengs-MacBook-Pro-2.local>
2024-02-08 21:08:54 -0500

    version the source code

--- practice/cmake/src/TestSome2/main.cpp
+++ practice/cmake/src/TestSome2/main.cpp
@@ -1,7 +1,9 @@
 #include "../SomeLibDemo/cpp/adder.h"
 #include <iostream>
 #include <GLFW/glfw3.h>
-int main() {
+#include <testsomeConfig.h>
+int main(int argc, char* argv[]) {
   std::cout << mearlymath:: add(2,3) << '\n';
+  std::cout << argv[0] << " version " << testsome_VERSION_MAJOR << "." << testsome_VERSION_MINOR << "\n";
   return 0;
  }
