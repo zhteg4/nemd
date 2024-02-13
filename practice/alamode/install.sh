3680b96a1a2feb5d55e1adc6c8ccde71438127d5
Teng Zhang <tzhang@Tengs-MacBook-Pro-2.local>
2024-02-13 10:14:59 -0500

    cleaning submodule

--- practice/alamode/install.sh
+++ practice/alamode/install.sh
@@ -9,18 +9,20 @@ brew install spglib
 brew install fftw
 brew install cmake
 brew install llvm
+
 git submodule add https://github.com/ttadano/alamode.git
-cd alamode
-git checkout develop
+git -C alamode checkout develop
 
 export CC=/usr/local/opt/llvm/bin/clang
 export SPGLIB_ROOT=/usr/local/Cellar/spglib/2.3.0
 export LD_LIBRARY_PATH=$SPGLIB_ROOT/lib:$LD_LIBRARY_PATH
 
-cd alm
-mkdir build; cd build
-cmake -DSPGLIB_ROOT=/usr/local/Cellar/spglib/2.3.0 -S ..
-make
+# cd alm
+# mkdir build; cd build
+# cmake -DSPGLIB_ROOT=/usr/local/Cellar/spglib/2.3.0 -S ..
+# make
 
-cmake -DUSE_MKL_FFT=yes -DSPGLIB_ROOT=/usr/local -DFFTW3_ROOT=/usr/local -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++  ..
+mkdir build; cd build
+cmake -DUSE_MKL_FFT=yes -DSPGLIB_ROOT=/usr/local -DFFTW3_ROOT=/usr/local -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++  ../alamode
+make
 
