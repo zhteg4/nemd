f09245be77da74e98819214e2d70bc00c38d2443
Teng Zhang <tzhang@Tengs-MacBook-Pro-2.local>
2024-02-14 12:12:19 -0500

    Silicon thermal conductivity by lammps and alamode works now

--- submodule/lammps/install.sh
+++ submodule/lammps/install.sh
@@ -5,6 +5,6 @@ brew install clang-format
 git submodule add -b develop https://github.com/lammps/lammps.git
 
 mkdir build; cd build
-cmake -D PKG_PYTHON=on -D PKG_MOLECULE=on -D PKG_KSPACE=on -D PKG_RIGID=on -D BUILD_LIB=on -D BUILD_SHARED_LIBS=yes -D PYTHON_EXECUTABLE="/usr/local/bin/python3" -D CMAKE_INSTALL_PREFIX=/usr/local -D LAMMPS_MACHINE=serial -S ../lammps/cmake/ -B .
+cmake -D PKG_PYTHON=on -D PKG_MANYBODY=on -D PKG_MOLECULE=on -D PKG_KSPACE=on -D PKG_RIGID=on -D BUILD_LIB=on -D BUILD_SHARED_LIBS=yes -D PYTHON_EXECUTABLE="/usr/local/bin/python3" -D CMAKE_INSTALL_PREFIX=/usr/local -D LAMMPS_MACHINE=serial -S ../lammps/cmake/ -B .
 cmake --build . -j4
 make install-python
